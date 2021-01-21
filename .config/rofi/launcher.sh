f52ba2f6589d66de71a218ac777655ef29ab2a7f
Josh <41928816+cmdywrtr27@users.noreply.github.com>
2021-01-21 11:27:13 -0700

    Create launcher.sh

---
+++ .config/rofi/launcher.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+theme="config"
+dir="$HOME/.config/rofi"
+
+# dark
+ALPHA="#00000000"
+BG="#222222ff"
+FG="#FFFFFFff"
+SELECT="#101010ff"
+
+# accent colors
+COLORS=('#EC7875' '#61C766' '#FDD835' '#42A5F5' '#BA68C8' '#4DD0E1' '#00B19F' \
+		'#FBC02D' '#E57C46' '#AC8476' '#6D8895' '#EC407A' '#B9C244' '#6C77BB')
+ACCENT="#42A5F5"
+
+# overwrite colors file
+cat > $dir/colors.rasi <<- EOF
+	/* colors */
+
+	* {
+	  al:  $ALPHA;
+	  bg:  $BG;
+	  se:  $SELECT;
+	  fg:  $FG;
+	  ac:  $ACCENT;
+	}
+EOF
+
+rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
