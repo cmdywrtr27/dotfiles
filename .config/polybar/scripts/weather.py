4926ef9e634bd8060b57cde895d75eb87472f1d9
Josh <41928816+cmdywrtr27@users.noreply.github.com>
2021-01-25 22:18:45 -0700

    Create weather.py

---
+++ .config/polybar/scripts/weather.py
@@ -0,0 +1,23 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+
+CITY = ""
+API_KEY = ""
+UNITS = "Imperial"
+#UNIT_KEY = "C"
+UNIT_KEY = "F"
+LANG = "en"
+
+REQ = requests.get("http://api.openweathermap.org/data/2.5/weather?id={}&lang={}&appid={}&units={}".format(CITY, LANG,  API_KEY, UNITS))
+try:
+    # HTTP CODE = OK
+    if REQ.status_code == 200:
+        CURRENT = REQ.json()["weather"][0]["description"].capitalize()
+        TEMP = int(float(REQ.json()["main"]["temp"]))
+        print("{}, {} °{}".format(CURRENT, TEMP, UNIT_KEY))
+    else:
+        print("Error: BAD HTTP STATUS CODE " + str(REQ.status_code))
+except (ValueError, IOError):
+    print("Error: Unable print the data")
