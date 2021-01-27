05bbb59251baa70a704b8f0ab49a44f62deb1b87
Josh <41928816+cmdywrtr27@users.noreply.github.com>
2021-01-26 19:58:18 -0700

    update rofi

--- .config/rofi/launcher.sh
+++ .config/rofi/launcher.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-theme="config"
+theme="style_5"
 dir="$HOME/.config/rofi"
 
 # dark
@@ -9,10 +9,17 @@ BG="#222222ff"
 FG="#FFFFFFff"
 SELECT="#101010ff"
 
+# light
+#ALPHA="#00000000"
+#BG="#FFFFFFff"
+#FG="#000000ff"
+#SELECT="#f3f3f3ff"
+
 # accent colors
 COLORS=('#EC7875' '#61C766' '#FDD835' '#42A5F5' '#BA68C8' '#4DD0E1' '#00B19F' \
 		'#FBC02D' '#E57C46' '#AC8476' '#6D8895' '#EC407A' '#B9C244' '#6C77BB')
 ACCENT="#42A5F5"
+#ACCENT="${COLORS[$(( $RANDOM % 14 ))]}ff"
 
 # overwrite colors file
 cat > $dir/colors.rasi <<- EOF
@@ -26,5 +33,3 @@ cat > $dir/colors.rasi <<- EOF
 	  ac:  $ACCENT;
 	}
 EOF
-
-rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
