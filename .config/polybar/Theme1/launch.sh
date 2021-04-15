ca7da072c7235429ac530623d6a6ec3b2f5c33f3
Josh <41928816+cmdywrtr27@users.noreply.github.com>
2021-04-14 21:29:29 -0700

    Update and rename launch.openbox.sh to launch.sh

---
+++ .config/polybar/Theme1/launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Top Bar
+polybar -c ~/.config/polybar/Theme1/config hdmi1 &
+polybar -c ~/.config/polybar/Theme1/config hdmi2 &
+
+# Bottom Bar
+#polybar -c ~/.config/polybar/Theme1/config bhdmi1 &
+#polybar -c ~/.config/polybar/Theme1/config bhdmi2 &
