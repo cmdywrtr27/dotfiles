42c8a55e3d01a429416b68dfc4326ee1de077b73
Josh <41928816+cmdywrtr27@users.noreply.github.com>
2021-07-24 13:06:33 -0700

    Update .prompt12.zsh

--- .prompt12.zsh
+++ .prompt12.zsh
@@ -9,11 +9,11 @@ fi
 local return_code="%(?..%{$fg[red]%} %? %{$reset_color%})"
 
 PROMPT='
-%F{blue}┌─── $(zsh_prompt_home_indicator)%~
-%F{blue}└%f '
+%F{blue}┌─── $(zsh_prompt_home_indicator)%~%{$reset_color%}
+%F{blue}└─%f%{$reset_color%} '
 RPROMPT='${return_code} $(git_prompt_info)'
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[magenta]%} %{$fg[blue]%}("
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%} "
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%}) %{$fg[green]%} "
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%} "
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%}) %{$fg[green]%} "
