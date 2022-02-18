b022ab7c86f2363eee31b42384bb74ce09b6dcc8
Josh <cmdywrtr27@gmail.com>
2022-02-17 22:18:32 -0700

    add arctic theme to polybar

---
+++ .config/polybar/arctic/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar -c ~/.config/polybar/arctic/config main &
+polybar -c ~/.config/polybar/arctic/config main2 &
+polybar -c ~/.config/polybar/arctic/config main3 &
