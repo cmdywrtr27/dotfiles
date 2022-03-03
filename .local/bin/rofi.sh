4b0d7a4039a5eaa44df4deac182e767eb2b8669a
Josh <cmdywrtr27@gmail.com>
2022-03-02 18:26:37 -0700

    add icons

---
+++ .local/bin/rofi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rofi -show drun \
+-modi drun \
+-show-icons -drun-icon-theme "Timber"
---
+++ .local/bin/rofi2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rofi -show drun \
+-modi drun \
+-theme $HOME/.config/rofi/themes/greywaves.rasi
