317b1889593dc1f4f2e13dfb0ca2b00dde711477
Josh <cmdywrtr27@gmail.com>
2022-03-04 10:35:13 -0700

    fix workspaces in polybar

--- .config/polybar/azul/scripts/uptime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# literaly the simplest script.......
-
-uptime --pretty | sed 's/up //' | sed 's/\ years\?,/y/' | sed 's/\ weeks\?,/w/' | sed 's/\ days\?,/d/' | sed 's/\ hours\?,\?/h/' | sed 's/\ minutes\?/m/'
