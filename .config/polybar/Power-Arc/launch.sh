67872573676f57e4026b1dabf5b17f61b7a6608b
Josh <cmdywrtr27@gmail.com>
2022-04-14 22:42:42 -0700

    add Power-Arc polybar theme

---
+++ .config/polybar/Power-Arc/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$HOME/.config/polybar/horizont"
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar -q main -c "$DIR"/config.ini &
+polybar -q main2 -c "$DIR"/config.ini &
+polybar -q main3 -c "$DIR"/config.ini &
